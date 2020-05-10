68379375ed435f816ae0d9b197b657f0ecb8450b
Rishu Gupta <rishurajgupta@gmail.com>
2020-05-10 15:40:52 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-custom-elements';
+  title = 'angular-micro-frontend';
 }
